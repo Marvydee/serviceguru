3041540ed5a30551f5044cea7f8b2ac9a56bbb69
the <miraclehycient@gmail.com>
2024-10-19 23:14:51 +0000

    Added content and styling to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,25 @@ import React from "react";
 
 function Footer() {
   return (
+	  <>
+      <div className="container">
+        <footer className="py-3 my-4">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li>
+              <a href="index.html" className="nav-link px-2 text-secondary">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="about.html" className="nav-link px-2 text-secondary">
+                About
+              </a>
+            </li>
+          </ul>
+          <p className="text-center text-body-secondary">© 2024 Serviceguru</p>
+        </footer>
+      </div>
+    </>
     
   );
 }
