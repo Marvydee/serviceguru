9c034a086d37de522ed516424ad0928231945cd7
franklin-dex <odinakachukwufranklin0@gmail.com>
2024-10-19 16:01:53 +0100

    add component-jsx file for selecting service types

---
+++ src/components/Categories.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Categories = ({ onCategorySelect }) => {
+	const categories = [
+		{ name: "Resturants", type: "resturnat" },
+		{ name: "Electrician", type: "electrician" },
+		{ name: "Mechanic", type: 'car_repair" },
+		{ name: "Plumbers", type: "plumber" },
+		{ name: "Laundary Service", type: "laundary" },
+		{ name: "Locksmith", type: "locksmith" },
+		{ name: "Painter", type: "painter" },
+		{ name: "veterinary Care", type: "veterinary_care" },
+	};
+
