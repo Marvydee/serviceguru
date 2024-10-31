2835c6a653fe80a1eeb8c4906a601bc5bba527fe
Marvydee <marvydee1@gmail.com>
2024-10-31 01:16:46 +0100

    modified API to update regularly

--- src/utils/loadAPI.js
+++ src/utils/loadAPI.js
@@ -3,7 +3,7 @@ const apiKey = process.env.REACT_APP_API_KEY;
 const loadGoogleMapsAPI = () => {
   return new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&v=3.49`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&v=weekly`;
     script.async = true;
     script.defer = true;
     script.onload = () => resolve();
